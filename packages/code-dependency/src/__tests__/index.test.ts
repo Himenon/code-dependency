8a8619bc9e5157e3d5403a339e557d2eab2b1339
Himenon <6715229+Himenon@users.noreply.github.com>
2019-04-14 14:24:42 +0900

    feat(code-dependency): update

--- packages/code-dependency/src/__tests__/index.test.ts
+++ packages/code-dependency/src/__tests__/index.test.ts
@@ -3,12 +3,18 @@ import * as path from "path";
 import { execute } from "../index";
 
 describe("#index", () => {
-  const baseDir = path.resolve(__dirname, "../../");
+  const baseDir = path.resolve(__dirname, "../");
   const options: Types.Options = {
     source: path.resolve(baseDir, "../index.ts"),
-    executeDirectory: "src",
+    executeDirectory: baseDir,
   };
-  test("#execute", () => {
-    execute(options, {});
+
+  test("#execute", async done => {
+    const result = await execute(options, {
+      symlinks: false,
+      extensions: [".js", ".jsx", ".ts", ".tsx"],
+    });
+    expect(result.length).toEqual(23);
+    done();
   });
 });

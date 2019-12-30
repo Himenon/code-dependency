20fff3ec168badd354c198bccc5c26948cb1e0ae
Himenon <k.himeno314@gmail.com>
2019-12-30 13:27:43 +0900

    build(webpack): update settings

--- packages/view/scripts/webpack/rules/index.ts
+++
@@ -1,9 +0,0 @@
-import * as AssetsLoader from "./AssetsLoader";
-import * as BabelLoader from "./BabelLoader";
-import * as TsLoader from "./TsLoader";
-
-export const rules = {
-  babelLoader: BabelLoader.generateRule,
-  tsLoader: TsLoader.generateRule,
-  assetsLoader: AssetsLoader.generateRule,
-};

ff78a780ce565c1d7d9bc0ccf7de159f80f775ab
Himenon <k.himeno314@gmail.com>
2020-01-03 22:53:44 +0900

    test: update test & update ci settings

--- packages/view/src/container/FileTree/__tests__/Store.test.ts
+++ packages/view/src/container/FileTree/__tests__/Store.test.ts
@@ -20,50 +20,58 @@ const filePathObjectList: FilePathObject[] = [
   },
 ];
 
-const directoryResult: FileTree.EuiSideNavItem = {
-  id: ".",
-  name: "",
-  items: [
-    {
-      id: "./a",
-      name: "a",
-      items: [
-        {
-          id: "./a/b",
-          name: "b",
-          items: [
-            {
-              id: "./a/b/c",
-              name: "c",
-              items: [
-                {
-                  id: "./a/b/c/index.ts",
-                  name: "index.ts",
-                },
-              ],
-            },
-            {
-              id: "./a/b/index.ts",
-              name: "index.ts",
-            },
-          ],
-        },
-        {
-          id: "./a/index.ts",
-          name: "index.ts",
-        },
-      ],
-    },
-    {
-      id: "./app.ts",
-      name: "app.ts",
-    },
-    {
-      id: "./index.ts",
-      name: "index.ts",
-    },
-  ],
-};
+const directoryResult: FileTree.EuiSideNavItem[] = [
+  {
+    id: ".",
+    name: "@code-dependency",
+    items: [
+      {
+        id: ".",
+        name: ".",
+        items: [
+          {
+            id: "./a",
+            name: "a",
+            items: [
+              {
+                id: "./a/b",
+                name: "b",
+                items: [
+                  {
+                    id: "./a/b/c",
+                    name: "c",
+                    items: [
+                      {
+                        id: "./a/b/c/index.ts",
+                        name: "index.ts",
+                      },
+                    ],
+                  },
+                  {
+                    id: "./a/b/index.ts",
+                    name: "index.ts",
+                  },
+                ],
+              },
+              {
+                id: "./a/index.ts",
+                name: "index.ts",
+              },
+            ],
+          },
+          {
+            id: "./app.ts",
+            name: "app.ts",
+          },
+          {
+            id: "./index.ts",
+            name: "index.ts",
+          },
+        ],
+      },
+    ],
+  },
+];
 
 describe("Store.ts", () => {
   test("generateFolderTree", () => {

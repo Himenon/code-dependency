35bd14866bb5f7edf7b03eefb4f4df7d144402b0
Himenon <k.himeno314@gmail.com>
2020-02-07 16:38:20 +0900

    feat: enabled render over 300kb dot source

--- packages/cli/src/service/Renderer.ts
+++ packages/cli/src/service/Renderer.ts
@@ -1,6 +1,9 @@
+import * as fs from "fs";
+import * as rimraf from "rimraf";
 import Viz from "viz.js";
 import { Module, render } from "viz.js/full.render.js";
 import { execSync } from "child_process";
+import tempfile from "tempfile";
 
 export interface Renderer {
   renderToString: (dotSource: string) => Promise<string>;
@@ -16,19 +19,37 @@ const createViz = (): Renderer => {
 };
 
 const createNativeDot = (): Renderer => {
+  const createSvgElementForLargeSize = (dotSource: string) => {
+    const dotSourceFile = tempfile(".dot");
+    const svgSourceFile = tempfile(".svg");
+    fs.writeFileSync(dotSourceFile, dotSource, { encoding: "utf-8" });
+    execSync(`dot -T svg ${dotSourceFile} > ${svgSourceFile}`);
+    const svgElement = fs.readFileSync(svgSourceFile, { encoding: "utf-8" });
+    rimraf.sync(dotSource);
+    rimraf.sync(svgSourceFile);
+    return svgElement;
+  };
+  const createSvgElementForSmallSize = (dotSource: string) => {
+    return execSync(`echo '${dotSource}' | dot -T svg`, { encoding: "utf-8" })
+      .toString()
+      .trim();
+  };
+  const thresholdBytes = 300000; // 300 KB
   return {
     renderToString: async (dotSource: string) => {
+      const stringSize = Buffer.byteLength(dotSource, "utf8");
       try {
-        return execSync(`echo '${dotSource}' | dot -T svg`, { encoding: "utf-8" })
-          .toString()
-          .trim();
+        if (stringSize > thresholdBytes) {
+          return createSvgElementForLargeSize(dotSource);
+        } else {
+          return createSvgElementForSmallSize(dotSource);
+        }
       } catch (error) {
-        const stringkb = Buffer.byteLength(dotSource, "utf8") / 1000;
         return `<h3>Transform Error!</h3>
           <ul>
             <li>Error Code: ${error.code}</li>
             <li>Reason : ${error.code === "E2BIG" ? "Very Large input !" : "unknown error."}</li>
-            <li>Size: ${stringkb} KB</li>
+            <li>Size: ${stringSize} KB</li>
           </ul>
           <section>
             <h3>dot source</h3>

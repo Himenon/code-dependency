4d98a83e5d671d0cf5ad41b7741b9136d59a053d
Himenon <k.himeno314@gmail.com>
2020-02-07 17:35:38 +0900

    docs(0.4.1): update docs

--- docs/assets/scripts/styles.35fad665f1.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{600:function(n,w,o){}}]);
---
+++ docs/assets/scripts/styles.e302b899f5.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{599:function(n,w,o){}}]);
